6860dd3b8e158182c50305a04df4cff082a0cc69
Paurush <40339719+paurush1710@users.noreply.github.com>
2023-06-06 00:24:51 +0530

    Create Course.java

---
+++ CourseAPI/Course.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Course {
+	
+	private long c_id;
+	private String c_name;
+	private String desc;
+	public long getC_id() {
+		return c_id;
+	}
+	public void setC_id(long c_id) {
+		this.c_id = c_id;
+	}
+	public String getC_name() {
+		return c_name;
+	}
+	public void setC_name(String c_name) {
+		this.c_name = c_name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	
+
+}
